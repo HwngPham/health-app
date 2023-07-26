7e468c557e8a885166650ec14dec1a1976865d41
hwng <quanghwng22@gmail.com>
2023-07-27 00:50:19 +0700

    Add fonts & define theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,32 @@
 export default {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      color: {
+        transparent: "transparent",
+        black: "#000",
+        light: "#fff",
+        dark: {
+          600: "#2E2E2E",
+          500: "#414141",
+        },
+        gray: {
+          400: "#777777",
+        },
+        primary: {
+          300: "#FFCC21",
+          400: "#414141",
+          500: "#414141",
+        },
+        secondary: {
+          300: "#8FE9D0",
+        },
+      },
+      fontFamily: {
+        primary: "NotoSansJP",
+        secondary: "Inter",
+      },
+    },
   },
   plugins: [],
 };
