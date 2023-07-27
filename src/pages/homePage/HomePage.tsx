cb9e4228ca82f063598fe58f95f89ec4156d1c63
hwng <quanghwng22@gmail.com>
2023-07-28 01:57:22 +0700

    Implement HomePage > MenuCategory

--- src/pages/homePage/HomePage.tsx
+++ src/pages/homePage/HomePage.tsx
@@ -1,6 +1,12 @@
 import { useState } from "react";
+import { MenuCategory } from "./components/MealCategory";
 
 export const HomePage = () => {
   const [mealRecords, setMealRecords] = useState([]);
-  return <p>HomePage</p>;
+  return (
+    <>
+      <p>HomePage</p>
+      <MenuCategory />
+    </>
+  );
 };
