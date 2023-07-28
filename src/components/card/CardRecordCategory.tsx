b784c357815557480b21a80dac07de9ef40cdc61
hwng <quanghwng22@gmail.com>
2023-07-29 00:45:15 +0700

    Implement RecordPage > Categories

---
+++ src/components/card/CardRecordCategory.tsx
@@ -0,0 +1,48 @@
+import { clsx } from "clsx";
+
+export interface CardRecordCategoryProps {
+  label: string;
+  info: string;
+  coverImg: string;
+}
+
+export const CardRecordCategory = ({
+  label,
+  info,
+  coverImg,
+}: CardRecordCategoryProps) => (
+  <div className="bg-primary-300 w-[288px] h-[288px] p-6 cursor-pointer">
+    <div className="bg-black h-full">
+      <div
+        className="h-full relative flex flex-col gap-2 justify-center items-center"
+        style={{
+          backgroundImage: `url(${coverImg})`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+        }}
+      >
+        <div className="absolute w-full h-full bg-black bg-opacity-75" />
+        <p
+          className={clsx(
+            "text-opacity-100",
+            "font-secondary uppercase",
+            "text-2xl leading-[30px] text-primary-300",
+            "z-10",
+          )}
+        >
+          {label}
+        </p>
+        <p
+          className={clsx(
+            "w-40 h-6 z-10",
+            "font-primary font-light leading-5",
+            "text-center text-light text-sm",
+            "bg-yellow",
+          )}
+        >
+          {info}
+        </p>
+      </div>
+    </div>
+  </div>
+);
--- src/pages/recordPage/components/RecordCategories.tsx
+++ src/pages/recordPage/components/RecordCategories.tsx
@@ -1,3 +1,27 @@
+import { CardRecordCategory } from "../../../components/card/CardRecordCategory";
+
+const categories = [
+  {
+    label: "body reccord",
+    info: "自分のカラダの記録",
+    coverImg: "/images/MyRecommend-1.jpg",
+  },
+  {
+    label: "my excercise",
+    info: "自分の運動の記録",
+    coverImg: "/images/MyRecommend-2.jpg",
+  },
+  {
+    label: "my diary",
+    info: "自分の日記",
+    coverImg: "/images/MyRecommend-3.jpg",
+  },
+];
+
 export const RecordCategories = () => (
-  <section id="record-categories">RecordCategories</section>
+  <section id="record-categories" className="my-14 flex justify-center gap-12">
+    {categories.map((category) => (
+      <CardRecordCategory key={category.label} {...category} />
+    ))}
+  </section>
 );
