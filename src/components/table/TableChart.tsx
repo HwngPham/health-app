b1bad75588fea5bd44cb4ea97a1968980e042974
hwng <quanghwng22@gmail.com>
2023-08-01 22:35:07 +0700

    Add multiple datasets support for TableChart

--- src/components/table/TableChart.tsx
+++ src/components/table/TableChart.tsx
@@ -9,26 +9,19 @@ import {
   ResponsiveContainer,
 } from "recharts";
 
-const data = [
-  { name: "6月", uv: 2400, pv: 2400, top: 3000, bottom: 100 },
-  { name: "7月", uv: 2200, pv: 2300, top: 3000, bottom: 0 },
-  { name: "8月", uv: 2000, pv: 1800, top: 3000, bottom: 0 },
-  { name: "9月", uv: 1800, pv: 2200, top: 3000, bottom: 0 },
-  { name: "10月", uv: 1600, pv: 2000, top: 3000, bottom: 0 },
-  { name: "11月", uv: 1200, pv: 1800, top: 3000, bottom: 0 },
-  { name: "12月", uv: 1000, pv: 1400, top: 3000, bottom: 0 },
-  { name: "1月", uv: 1000, pv: 1700, top: 3000, bottom: 0 },
-  { name: "2月", uv: 900, pv: 1000, top: 3000, bottom: 0 },
-  { name: "3月", uv: 700, pv: 800, top: 3000, bottom: 0 },
-  { name: "4月", uv: 400, pv: 600, top: 3000, bottom: 0 },
-  { name: "5月", uv: 400, pv: 600, top: 3000, bottom: 0 },
-];
-
 export interface TableChartProps {
   label: string;
   subLabel: string;
+  dataSets: any;
+  currentDataSet: string;
 }
-export const TableChart = ({ label, subLabel }: TableChartProps) => (
+
+export const TableChart = ({
+  label,
+  subLabel,
+  dataSets,
+  currentDataSet,
+}: TableChartProps) => (
   <>
     <div
       className={clsx("flex items-center", "pl-6 pt-4 pb-[2px]", "text-white")}
@@ -54,7 +47,10 @@ export const TableChart = ({ label, subLabel }: TableChartProps) => (
 
     <div className="flex justify-center">
       <ResponsiveContainer width={880} height={210}>
-        <LineChart data={data} margin={{ bottom: 8, right: 10, left: 10 }}>
+        <LineChart
+          data={dataSets[currentDataSet]}
+          margin={{ bottom: 8, right: 10, left: 10 }}
+        >
           <Tooltip />
           <CartesianGrid
             stroke="#777"
